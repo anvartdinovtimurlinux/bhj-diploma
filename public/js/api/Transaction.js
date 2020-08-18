b86161db83a7297a380134d566f4f8d44d70b57d
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-08-18 09:58:06 +0300

    Написал классы Account, Transaction

--- public/js/api/Transaction.js
+++ public/js/api/Transaction.js
@@ -1,7 +1,9 @@
+'use strict';
 /**
  * Класс Transaction наследуется от Entity.
  * Управляет счетами пользователя.
  * Имеет свойство URL со значением '/transaction'
  * */
-class Transaction {
+class Transaction extends Entity {
+  static URL = "/transaction";
 }
