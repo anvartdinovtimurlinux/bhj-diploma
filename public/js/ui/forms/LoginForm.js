e714251d16dc9c08582170afd92829b9c4f66c97
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-08-18 12:20:03 +0300

    Взаимодействие API с пользовательским интерфейсом. Авторизация

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * Класс LoginForm управляет формой
  * входа в портал
@@ -10,7 +11,13 @@ class LoginForm {
    * устанавливает состояние App.setState( 'user-logged' ) и
    * закрывает окно, в котором находится форма
    * */
-  onSubmit( options ) {
-
+  onSubmit(options) {
+    User.login(options.data, (err, response) => {
+      if (response && response.success) {
+        this.element.reset();
+        App.setState('user-logged');
+        App.getModal('login').close();
+      }
+    });
   }
 }
